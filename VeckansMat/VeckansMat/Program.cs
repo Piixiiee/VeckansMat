3723eeee39c08cfb328f42b4b0e8a8d82b59ad3f
Jennifer <jennifer.l.hallberg@gmail.com>
2021-01-16 19:41:45 +0100

    Almost at MVP
    Still getting duplicate meals, incomplete allMeals list

--- VeckansMat/VeckansMat/Program.cs
+++ VeckansMat/VeckansMat/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VeckansMat
 {
     class Program
     {
+
+         
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello mat!");
+
+            var allMeals = new List<string> { "Spaghetti med sojafärssås", "Currygryta med ris", "Halloumistroganoff med ris",};
+            var random = new Random();
+            bool satisfied = false;
+
+            Console.WriteLine("Hur många rätter vill du ha?");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            while (!satisfied)
+            {
+                Console.WriteLine("Här är matlistan:");
+                for (int i = 0; i < amount; i++)
+                {
+                    int index = random.Next(allMeals.Count);
+                    Console.WriteLine(i + 1 + ". " + allMeals[index]);
+                }
+
+                Console.WriteLine("Är du nöjd med matlistan? y/n ");
+                string answer = Console.ReadLine();
+                if (answer == "y")
+                {
+                    satisfied = true;
+                    Console.WriteLine("Varsågod!");
+                }
+            }
         }
     }
 }
